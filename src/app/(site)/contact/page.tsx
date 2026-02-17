24b257637055434a2edc4d771f64ddc19697c4fd
Hardik <hardikgupta1999@gmail.com>
2026-02-16 16:17:01 -0800

    add contact banner

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     return (
         <main>
             <Herobanner
-                bannerimage="/images/contact/banner/contact-banner.png"
+                bannerimage="/images/contact/banner/contact-banner.jpg"
                 heading="Contact"
                 desc="Ready to <span>start something</span> great? Reach out  we’d love to hear from you." />
             <Contact contactdataNumber="01"/>
